10ce8142f1058e5bf14cdcff9ce7125c319e881e
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-04-22 20:08:30 -0300

    removendo pedidos do sistema

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -33,7 +33,7 @@
                             {{ s.tipo }}
                         </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
 
             </div>
@@ -67,7 +67,16 @@ export default {
             const req = await fetch('http://localhost:3000/status')
             const data = await req.json()
             this.status = data
+        },
 
+        async deleteBurger(id) {
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: 'DELETE'
+            })
+
+            const res = await req.json()
+
+            this.getPedidos()
         }
     },
 
