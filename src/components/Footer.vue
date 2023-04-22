d69f38fbb8708491d44631f570898f45459f2028
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-04-21 23:55:08 -0300

    criando formulario de cadastro de burger

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,3 +9,16 @@ export default {
   name: "Footer",
 };
 </script>
+
+<style scoped>
+#footer {
+  width: 100%;
+  height: 200px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
